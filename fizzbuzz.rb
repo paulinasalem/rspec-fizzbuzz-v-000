c5d2e4fcffc2c5e47133c39b3f2f0d4a312dfb28
Paulina Salem <paulinasalem@yahoo.com>
2018-04-02 22:30:34 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 def fizzbuzz(n)
-  if n % 3 == 0
+  if n % 5 == 0 && n % 5 == 0
+    puts "FizzBuzz"
+  elsif n % 3 == 0
 puts "Fizz"
 elsif n % 5 == 0
   puts "Buzz"
-elsif n % 5 == 0 && n % 5 == 0
-  puts "FizzBuzz"
 else
   puts "nil"
 end
-end 
+end
